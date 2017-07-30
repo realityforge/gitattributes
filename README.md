f2871ca66bf8693ebd38b46a526d55a832b1d542
Peter Donald <peter@realityforge.org>
2017-07-31 09:33:43 +1000

    Move the attribute parsing code to separate class and add license banner to indicate that this code was inherited from elsewhere

--- README.md
+++ README.md
@@ -10,9 +10,3 @@ A simple example of it's usage:
  attributes = Reality::GitAttributes.new('/home/user/myrepo')
  attributes.attributes('README.md') # => { "eol" => "lf }
 ```
-
-## Credit
-
-These classes were extracted from the [gitlab_git](https://gitlab.com/ben.boeckel/gitlab_git) by
-with the gitattribtues code originally authored by Douwe Maan. All credit goes to the original
-authors.
--- lib/reality/gitattributes.rb
+++ lib/reality/gitattributes.rb
@@ -1,10 +1,12 @@
+require 'reality/git/attributes_parser'
+
 module Reality
   # Class for parsing Git attribute files and extracting the attributes for
   # file patterns.
   #
   # Basic usage:
   #
-  #     attributes = Reality::Git::Attributes.new(some_repo.path)
+  #     attributes = Reality::Git::Attributes.new(some_repo_path)
   #
   #     attributes.attributes('README.md') # => { "eol" => "lf" }
   class GitAttributes
@@ -13,7 +15,8 @@ module Reality
     def initialize(repository_path, attributes_file = nil)
       @path = File.expand_path(repository_path)
       @attributes_file = attributes_file || "#{@path}/.gitattributes"
-      @patterns = nil
+      @patterns =
+        File.exist?(@attributes_file) ? Reality::Git::AttributesParser.parse_file(@attributes_file) : {}
     end
 
     # Returns all the Git attributes for the given path.
@@ -24,7 +27,7 @@ module Reality
     def attributes(path)
       full_path = File.join(@path, path)
 
-      self.patterns.each do |pattern, attrs|
+      @patterns.each do |pattern, attrs|
         return attrs if File.fnmatch?(File.join(@path, pattern), full_path)
       end
 
@@ -33,90 +36,7 @@ module Reality
 
     # Returns a Hash containing the file patterns and their attributes.
     def patterns
-      @patterns ||= parse_file
-    end
-
-    private
-
-    # Parses an attribute string.
-    #
-    # These strings can be in the following formats:
-    #
-    #     text      # => { "text" => true }
-    #     -text     # => { "text" => false }
-    #     key=value # => { "key" => "value" }
-    #
-    # string - The string to parse.
-    #
-    # Returns a Hash containing the attributes and their values.
-    def parse_attributes(string)
-      values = {}
-      dash = '-'
-      equal = '='
-      binary = 'binary'
-
-      string.split(/\s+/).each do |chunk|
-        # Data such as "foo = bar" should be treated as "foo" and "bar" being
-        # separate boolean attributes.
-        next if chunk == equal
-
-        key = chunk
-
-        # Input: "-foo"
-        if chunk.start_with?(dash)
-          key = chunk.byteslice(1, chunk.length - 1)
-          value = false
-
-          # Input: "foo=bar"
-        elsif chunk.include?(equal)
-          key, value = chunk.split(equal, 2)
-
-          # Input: "foo"
-        else
-          value = true
-        end
-
-        values[key] = value
-
-        # When the "binary" option is set the "diff" option should be set to
-        # the inverse. If "diff" is later set it should overwrite the
-        # automatically set value.
-        values['diff'] = false if key == binary && value
-      end
-
-      values
-    end
-
-    # Iterates over every line in the attributes file.
-    def each_line
-      full_path = @attributes_file
-
-      return unless File.exist?(full_path)
-
-      File.open(full_path, 'r') do |handle|
-        handle.each_line do |line|
-          yield line.strip
-        end
-      end
-    end
-
-    # Parses the Git attributes file.
-    def parse_file
-      pairs = []
-      comment = '#'
-
-      each_line do |line|
-        next if line.start_with?(comment) || line.empty?
-
-        pattern, attrs = line.split(/\s+/, 2)
-
-        parsed = attrs ? parse_attributes(attrs) : {}
-
-        pairs << [pattern, parsed]
-      end
-
-      # Newer entries take precedence over older entries.
-      pairs.reverse.to_h
+      @patterns.dup
     end
   end
 end
